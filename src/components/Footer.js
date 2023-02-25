c2323344a0fbc06e17a4ff4a7e4ed8e04d39e7c8
AymenKhanfir <aymenkhanfir56@gmail.com>
2023-02-25 19:19:41 +0100

    nothing new

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer(props) {
+  const bag = {
+    padding: "20px",
+    border: "1px solid gray",
+    background: "#fff",
+    margin: "20px 0"
+  }
+  return (
+      <div style = {bag}>
+          {props.children}
+      </div>
+  );
+};
+
+export default Footer;
